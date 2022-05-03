b9a15f193a778b42ebb4ff4df39e9dd762ca7c1a
Vadim <manjavha@gmail.com>
2022-05-03 10:59:56 +0300

    Initial commit

---
+++ src/test/java/pro/sky/java/course2/employee_book/EmployeeBookApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.employee_book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
